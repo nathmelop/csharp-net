9444d111490aeb3c10831f86281397e9cef07490
nathmelop <nathaliapmelo@hotmail.com>
2020-12-28 11:52:10 -0300

    Start

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int segundaGuerraMundial = 1942; // numero inteiro, não utiliza aspas 
+            String corFavorita = "azul"; //usa " " para identificar que é um texto
+            float velocidadeF1 = 294.98f; // necessario colocar o f no final 
+            bool segundaGuerraMundialAconteceu = true; //não pode ter o mesmo nome da variavel no mesmo lugar 
+
+            /*
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(segundaGuerraMundialAconteceu);
+
+            velocidadeF1 = 398.56f;
+            corFavorita = "roxo";
+
+            Console.WriteLine(velocidadeF1);
+            Console.WriteLine(corFavorita);
+            */
+            /*
+            var corFavorita = "azul"; 
+            var modeloDoProduto = 1996; 
+            */
+
+            /* dynamic corFavorita = "azul"; //variavel dinamica. parece pratico, mas não é 
+
+           Console.WriteLine(corFavorita);
+
+           corFavorita = 15151545;
+
+           Console.WriteLine(corFavorita);
+
+           corFavorita = 16.96f; 
+
+            const float PI = 3.4545454f; // quando é criado uma constante, o valor não pode ser mudado.  */
+
+            // Console.WriteLine("escreva seu nome:");
+            //string nome = Console.ReadLine(); /* ou  string nome = "";  nome = Console.ReadLine();     */
+            // Console.WriteLine("seu nome é: ");
+            //  Console.WriteLine(nome); */ 
+
+            int numeroQualquer = 24 + 50;
+            int numeroMult = 50 * 30;
+            float numeroDiv = 773f/ 3;
+            int teste = (5 + 3) * 4;
+            Console.WriteLine(numeroQualquer);
+            Console.WriteLine(numeroMult);
+            Console.WriteLine(numeroDiv);
+            Console.WriteLine(teste);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
